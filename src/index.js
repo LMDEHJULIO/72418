bbcef0de1829c36a5427febdb8286e202cd46027
LMDEHJULIO <juliodevelops@gmail.com>
2018-08-08 15:42:25 -0400

    Video prop and selection handler passed

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 import ReactDOM from "react-dom";
-import React from "react";
+import React, { Component } from "react";
 import SearchBar from "./components/search_bar";
 import key from "./../config.js";
 import YTSearch from "youtube-api-search";
+import VideoList from "./components/video_list";
+import VideoDetail from './components/video_detail'
 
 const API_KEY = key.YT_api;
 
-YTSearch({ key: API_KEY, term: "surfboards" }, function(data) {
-  console.log(data);
-});
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector(".container"));
